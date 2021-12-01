4587a1e1178e80559c95c29f9450a30339b8abf8
charlotte <charlotte.finance@etu.univ-lyon1.fr>
2021-12-01 17:24:00 +0100

    ✨Move and SendMessage methods

--- src/main/java/com/example/tp_sma_aknine/Agent.java
+++ src/main/java/com/example/tp_sma_aknine/Agent.java
@@ -1,15 +1,12 @@
 package com.example.tp_sma_aknine;
 
-import java.util.LinkedList;
-import java.util.Random;
-
 public class Agent {
 
     private String image; //path to the image
     private int x;
     private int y;
-    private int xGoal;
-    private int yGoal;
+    private final int xGoal;
+    private final int yGoal;
     private Environment environment;
     private MailBox mailBox;
 
@@ -27,76 +24,32 @@ public class Agent {
 
     }
 
-    public void action(Environment environment) {
 
-        perception(environment);
-
-        //MOVE
-        int xMove = new Random().nextInt(2);
-        int yMove = new Random().nextInt(2);
-        while (xMove + yMove < 1) {
-            xMove = new Random().nextInt(2);
-            yMove = new Random().nextInt(2);
+    public void move (int x, int y) {
+        Agent agent = this.environment.getContent(x,y);
+        if (agent == null){
+            this.environment.updateMap(this, this.x, this.y, x, y);
+            this.x = x;
+            this.y = y;
         }
-        int xDirection = new Random().nextInt(2);
-        int yDirection = new Random().nextInt(2);
-
-        if (xDirection == 0) {
-            if (environment.findAgent(this)[0] - xMove < 0) {
-                return;
-            }
-            if (yDirection == 0) {
-                if (environment.findAgent(this)[1] - yMove < 0) {
-                    return;
-                }
-                if (environment.isFreeOfAgent(environment.findAgent(this)[0] - xMove, environment.findAgent(this)[1] - yMove)) {
-                    environment.moveAgent(this, environment.findAgent(this)[0] - xMove, environment.findAgent(this)[1] - yMove);
-                }
-            } else {
-                if (environment.findAgent(this)[1] + yMove >= environment.getLargeMap()) {
-                    return;
-                }
-                if (environment.isFreeOfAgent(environment.findAgent(this)[0] - xMove, environment.findAgent(this)[1] + yMove)) {
-                    environment.moveAgent(this, environment.findAgent(this)[0] - xMove, environment.findAgent(this)[1] + yMove);
-                }
-            }
-        } else {
-            if (environment.findAgent(this)[0] + xMove >= environment.getLongMap()) {
-                return;
-            }
-            if (yDirection == 0) {
-                if (environment.findAgent(this)[1] - yMove < 0) {
-                    return;
-                }
-                if (environment.isFreeOfAgent(environment.findAgent(this)[0] + xMove, environment.findAgent(this)[1] - yMove)) {
-                    environment.moveAgent(this, environment.findAgent(this)[0] + xMove, environment.findAgent(this)[1] - yMove);
-                }
-            } else {
-                if (environment.findAgent(this)[1] + yMove >= environment.getLargeMap()) {
-                    return;
-                }
-                if (environment.isFreeOfAgent(environment.findAgent(this)[0] + xMove, environment.findAgent(this)[1] + yMove)) {
-                    environment.moveAgent(this, environment.findAgent(this)[0] + xMove, environment.findAgent(this)[1] + yMove);
-                }
-            }
+        else {
+            this.mailBox.addMessage(agent, x, y);
         }
     }
 
-    public boolean move (int x, int y) {
-        return true;
-    }
-
-    public boolean sendMessage(Agent target, int freeX, int freeY) {
-        mailBox.addMessage(target, freeX, freeY);
-        return true;
-    }
 
     public boolean checkMailBox() {
-        LinkedList<Message> messages = mailBox.deliverMessages(this);
-        return true;
+        for (Message message : mailBox.deliverMessages(this)){
+            if (message.getFreeX() == this.x && message.getFreeY() == this.y){
+                return true;
+            }
+        }
+        return false;
     }
 
-    public boolean checkPersonalGoal() { return (this.x == this.xGoal && this.y == this.yGoal); }
+    public boolean checkPersonalGoal() {
+        return (this.x == this.xGoal && this.y == this.yGoal);
+    }
 
     public boolean checkGlobalGoal() {
         for (Agent agent : environment.getListeAgents()) {
