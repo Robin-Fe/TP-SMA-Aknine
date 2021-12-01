24b1980569dd7bd2b98405149a1cb59eb0165b04
charlotte <charlotte.finance@etu.univ-lyon1.fr>
2021-12-01 16:45:57 +0100

    ✨ Classes Message and MailBox

---
+++ src/main/java/com/example/tp_sma_aknine/MailBox.java
@@ -0,0 +1,23 @@
+package com.example.tp_sma_aknine;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class MailBox {
+    private final HashMap<Agent, LinkedList<Message>> box = new HashMap<>();
+
+    public MailBox() {}
+
+    public void addMessage(Agent target, int freeX, int freeY) {
+        this.box.computeIfAbsent(target, k -> new LinkedList<>());
+        this.box.get(target).add(new Message(freeX, freeY));
+    }
+
+    public LinkedList<Message> deliverMessages(Agent checker) {
+        LinkedList<Message> messages = this.box.get(checker);
+        this.box.remove(checker);
+        return messages;
+    }
+
+
+}
---
+++ src/main/java/com/example/tp_sma_aknine/Message.java
@@ -0,0 +1,12 @@
+package com.example.tp_sma_aknine;
+
+public class Message {
+    private final int freeX;
+    private final int freeY;
+
+    public Message(int freeX, int freeY) {
+        this.freeX = freeX;
+        this.freeY = freeY;
+    }
+
+}
